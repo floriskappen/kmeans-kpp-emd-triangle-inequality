ea438f63827169690c4f94ee5766b810e4b6b6be
Floris <kappenfloris@gmail.com>
2024-04-22 17:40:14 +0200

    hotfix: bug with inertia

--- src/inertia.rs
+++ src/inertia.rs
@@ -2,7 +2,6 @@ use crate::distance::{earth_movers_distance, euclidian_distance};
 
 
 pub fn calculate_inertia_emd(data: &[u8], histogram_size: usize, centroids: &[Vec<f64>], labels: &[u32]) -> f64 {
-    let histogram_size = 8;
     let result = labels.iter().enumerate().map(|(i, &label)| {
         let point_start = i * histogram_size;
         let point = &data[point_start..point_start + histogram_size];
@@ -18,7 +17,6 @@ pub fn calculate_inertia_emd(data: &[u8], histogram_size: usize, centroids: &[Ve
 }
 
 pub fn calculate_inertia_emd_f64(data: &[f64], histogram_size: usize, centroids: &[Vec<f64>], labels: &[u32]) -> f64 {
-    let histogram_size = 8;
     let result = labels.iter().enumerate().map(|(i, &label)| {
         let point_start = i * histogram_size;
         let point = &data[point_start..point_start + histogram_size].to_vec();
